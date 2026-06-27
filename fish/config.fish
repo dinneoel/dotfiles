fe9e11df9430b8015d4c2587b82687b1f77345b0
Din Noel <dinnoel@pm.me>
2026-06-27 19:32:24 +0000

    update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,8 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    export PATH="$PATH:/opt/nvim-linux-x86_64/bin"
+    export PATH="$PATH:$HOME/.local/bin"
+
     starship init fish | source
     zoxide init fish | source
     fzf --fish | source
-    export PATH="$PATH:/opt/nvim-linux-x86_64/bin"
-    export PATH="$PATH:$HOME/.local/bin"
 end
